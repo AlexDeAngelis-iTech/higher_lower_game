3e22ee041b97002bc47f1f404dba053455bc28f4
Alex De Angelis <alex.deangelis@itech.media>
2022-05-23 17:25:06 +0100

    evaluate previous and current card values and display result

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -5,8 +5,9 @@
     <p>Render the number on button click: {{ showButtonNumber }}</p>
     <button v-on:click="getButtonNumber">Get a number!</button>
     <hr />
-    <p>Get a new card from the array on button click: {{ cardValue }} {{ testValue }}</p>
+    <p>Get a new card from the array on button click: {{ cardValue }}</p>
     <button v-on:click="getCard">Get a card!</button>
+    <p>Result: {{ result }}</p>
   </div>
 </template>
 
@@ -19,7 +20,9 @@ export default {
       text: "Hello!",
       showButtonNumber: "",
       cardValue: "",
-      testValue: ""
+      currentValue: "",
+      previousValue: "",
+      result: "",
     };
   },
   methods: {
@@ -31,28 +34,48 @@ export default {
     getButtonNumber() {
       this.showButtonNumber = Math.ceil(Math.random() * 100);
     },
-    aValue() {
-      return 'hello';
+    evaluateCardValues() {
+      if (this.currentValue && this.previousValue) {
+        if (this.currentValue > this.previousValue) {
+          // Higher
+          return 'Higher! As ' + this.currentValue + ' is more than ' + this.previousValue + '!';
+        } else if (this.currentValue < this.previousValue) {
+          // Lower
+          return 'Lower! As ' + this.currentValue + ' is less than ' + this.previousValue + '!';
+        } else {
+          // Draw
+          return 'Draw! As ' + this.currentValue + ' is the same as ' + this.previousValue + '!';
+        }
+      } else {
+        return 'not enough data!';
+      }
     },
     getCard() {
+
+      // If a currentValue exists, now set it as the previousValue
+      if (this.currentValue) {
+        this.previousValue = this.currentValue;
+      }
+
       // Check if array is empty here
       if (numbers.length > 0) {
         // Shuffle the parent array
         const randomCard = numbers.sort(() => Math.random() - 0.5);
         // Get the random number
         let number = randomCard[0][0];
-
+        // Set currentValue as the number
+        this.currentValue = number;
         // Convert number to string if necessary
-        if (number == "1") {
+        if (number === 1) {
           number = 'Ace';
-        } else if (number == '11') {
+        } else if (number === 11) {
           number = 'Jack'
-        } else if (number == '12') {
+        } else if (number === 12) {
           number = 'Queen'
-        } else if (number == '13') {
+        } else if (number === 13) {
           number = 'King'
         }
-        this.testValue = this.aValue();
+        this.result = this.evaluateCardValues();
         const suits = randomCard[0][1];
 
         // Check if the suits array is empty here
