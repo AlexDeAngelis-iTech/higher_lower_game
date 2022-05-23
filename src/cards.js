a617fd3f04a3b31d3b2b1cfb91dc794aea8e7602
Alex De Angelis <alex.deangelis@itech.media>
2022-05-23 17:22:30 +0100

    changing card numbers from string to integer

--- src/cards.js
+++ src/cards.js
@@ -8,19 +8,45 @@
 // 13 = King
 
 const numbers = [
-  ['1', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['2', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['3', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['4', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['5', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['6', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['7', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['8', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['9', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['10', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['11', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['12', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
-  ['13', ['Hearts', 'Diamonds', 'Clubs', 'Spades']],
+  [1,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [2,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [2,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [4,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [5,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [6,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [7,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [8,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [9,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [10,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [11,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [12,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
+  [13,
+    ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  ],
 ];
 
 
